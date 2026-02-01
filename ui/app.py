9af5e389d1a219452201469d7023553fab74e3c4
Shubham Kumar <shubhamkr17074@gmail.com>
2026-02-01 16:40:52 +0530

    update app.py

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://fraud-detection-tixp.onrender.com"
 
 st.set_page_config(
     page_title="Fraud Detection System",
